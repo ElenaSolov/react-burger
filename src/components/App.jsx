47779d4e04b376cbfb649dd24a843c561ad9a22f
Elena Soloveva <seaver@yandex.ru>
2022-03-15 13:40:36 +0200

    feat: add fonts

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppHeader from "./appHeader/AppHeader";
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader></AppHeader>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/appHeader/AppHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppHeader = () => {
+  return (
+    <header>
+      Privet
+    </header>
+  );
+};
+
+export default AppHeader;
