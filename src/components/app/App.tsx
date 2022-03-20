af52c451af19b9ed73f377525546636cf7de2f25
Elena Soloveva <seaver@yandex.ru>
2022-03-20 13:32:56 +0200

    feat: add ConstructorEl component and styles

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import appStyles from './app.module.css';
+import AppHeader from "../appHeader/AppHeader";
+import BurgerIngredients from "../burgerIngredients/BurgerIngredients";
 
 function App() {
   return (
