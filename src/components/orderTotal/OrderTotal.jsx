4f917d555bcd767f35a042df4fa9fa499224461e
Elena Soloveva <seaver@yandex.ru>
2022-03-20 13:38:57 +0200

    feat: add OrderTotal component and styles

---
+++ src/components/orderTotal/OrderTotal.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {CurrencyIcon} from "@ya.praktikum/react-developer-burger-ui-components";
+import {Button} from "@ya.praktikum/react-developer-burger-ui-components";
+import orderTotalStyles from './orderTotal.module.css'
+
+class OrderTotal extends React.Component{
+  render() {
+    return (
+      <div className={`${orderTotalStyles.orderTotal} mt-10`}>
+        <p className={`text text_type_digits-medium mr-10`}>0<span className={orderTotalStyles.priceIcon}><CurrencyIcon type='primary' /></span></p>
+        <Button type="primary" size="medium">
+          Оформить заказ
+        </Button>
+      </div>
+    );
+  }
+};
+
+export default OrderTotal;
