ad739b287f688fbafc2f36158852da683bf105d6
Elena Soloveva <seaver@yandex.ru>
2022-03-20 13:20:19 +0200

    feat: add NavBar and NavItem component and styles

---
+++ src/components/navBar/NavBar.tsx
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import NavItem from "../NavItem/NavItem";
+import {BurgerIcon, ListIcon} from "@ya.praktikum/react-developer-burger-ui-components";
+import navBarStyles from './navBar.module.css'
+
+class NavBar extends Component {
+
+    render() {
+        return (
+            <nav className={navBarStyles.navBar}>
+                <ul className={navBarStyles.navlist}>
+                    <li>
+                        <NavItem value="Конструктор" text='Конструктор'><BurgerIcon type="primary"/></NavItem>
+                    </li>
+                    <li>
+                        <NavItem value="Лента" text='Лента заказов'><ListIcon type="primary"/></NavItem>
+                    </li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default NavBar;
