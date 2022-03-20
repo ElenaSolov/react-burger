11206fa76efd85e3f46947a0611a9cc611c3a120
Elena Soloveva <seaver@yandex.ru>
2022-03-20 13:20:19 +0200

    feat: add NavBar and NavItem component and styles

---
+++ src/components/navItem/NavItem.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import navItemStyles from './navItem.module.css'
+
+class NavItem extends React.Component {
+    constructor(props) {
+        super();
+        this.props = props;
+    }
+    render() {
+        (
+            <a className={`${navItemStyles.navItem} mb-4 mt-4 mr-2 p-5`}>
+                {this.props.children}
+                <p className="text text_type_main-default ml-2">{this.props.text}</p>
+            </a>
+        );
+    }
+}
