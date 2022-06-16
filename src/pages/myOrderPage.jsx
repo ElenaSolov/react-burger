6895637849251c23f8c98b5da7432a8e7441241a
Elena Soloveva <seaver@yandex.ru>
2022-06-16 10:09:14 +0300

    fix: get id with useParams

--- src/pages/myOrderPage.jsx
+++ src/pages/myOrderPage.jsx
@@ -1,20 +1,17 @@
 import React, { useEffect } from "react";
 import pagesStyles from "./pages.module.css";
-import { useLocation } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import OrderFeedDetails from "../components/orderFeedDetails/OrderFeedDetails";
 import { getOrderDetails } from "../services/actions/actions";
 
 function MyOrderPage() {
-  const location = useLocation();
   const orderFailed = useSelector((store) => store.order.orderFailed);
-  console.log(orderFailed);
-  const locationArr = location.pathname.split("/");
-  const number = locationArr[locationArr.length - 1];
+  const { id } = useParams();
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getOrderDetails(number));
-  }, [dispatch, number]);
+    dispatch(getOrderDetails(id));
+  }, [dispatch, id]);
   const order = useSelector((store) => store.order.order);
   return (
     <section className={pagesStyles.page}>
