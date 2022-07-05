c4ddc378c3e618aff42d021629cdcb1272b2dcba
Elena Soloveva <seaver@yandex.ru>
2022-07-05 11:57:11 +0300

    feat: add declaration for png convert OrderDetails into tsx

--- src/components/orderDetails/OrderDetails.tsx
+++ src/components/orderDetails/OrderDetails.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { FC } from "react";
 import orderDetailsStyles from "./orderDetails.module.css";
 import img from "../../images/order-done.png";
-import { useSelector } from "react-redux";
+import { useSelector } from "../../services/hooks";
 
-const OrderDetails = () => {
+const OrderDetails: FC = () => {
   const orderNum = useSelector((store) => store.order.orderNum);
   return (
     <>
