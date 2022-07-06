bc87b31395466b42db049807c0baaba306c8d31b
Elena Soloveva <seaver@yandex.ru>
2022-07-06 10:27:30 +0300

    feat: convert home page to tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,7 +3,7 @@ import BurgerIngredients from "../components/burgerIngredients/BurgerIngridients
 import BurgerConstructor from "../components/burgerConstructor/BurgerConstructor";
 import appStyles from "../components/app/app.module.css";
 
-export function HomePage() {
+export function HomePage(): JSX.Element {
   return (
     <main className={appStyles.main}>
       <div className={appStyles.container}>
