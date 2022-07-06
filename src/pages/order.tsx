93901397543f671c804a0f83878e4b45f7cd7b0c
Elena Soloveva <seaver@yandex.ru>
2022-07-06 10:50:11 +0300

    feat: convert order page to tsx

--- src/pages/order.tsx
+++ src/pages/order.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from "react";
 import pagesStyles from "./pages.module.css";
 import { useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "../services/hooks";
 import OrderFeedDetails from "../components/orderFeedDetails/OrderFeedDetails";
 import { getOrderDetails } from "../services/actions/actions";
+import { IOrder } from "../services/types/data";
 
-function OrderPage() {
+function OrderPage(): JSX.Element {
   const { id } = useParams();
   const dispatch = useDispatch();
   useEffect(() => {
@@ -14,11 +15,15 @@ function OrderPage() {
   const order = useSelector((store) => store.order.order);
   const orderFailed = useSelector((store) => store.order.orderFailed);
 
+  function isOrderFound(order: IOrder | object): order is IOrder {
+    return "number" in order;
+  }
+
   return (
     <section className={pagesStyles.page}>
       {orderFailed ? (
         <h1 className="text text_type_main-large">Что-то пошло не так...</h1>
-      ) : order && order.number ? (
+      ) : isOrderFound(order) ? (
         <OrderFeedDetails order={order} />
       ) : (
         <h1 className="text text_type_main-large">
